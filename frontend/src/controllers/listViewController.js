b94c0afbc09ae55d0418394b9d3e06801ecea3c0
Rolf Erik Lekang <me@rolflekang.com>
2013-12-19 09:30:32 +0100

    add warning for bad user id

--- frontend/src/controllers/listViewController.js
+++ frontend/src/controllers/listViewController.js
@@ -13,10 +13,25 @@ app.controller('listViewController', ['$scope', function ($scope) {
     }
   };
 
+  $scope.check_user_id = function (user_id){
+    if (user_id) {
+      return user_id.match(/^ur\d{8}$/) !== null;
+    }
+    return user_id;
+  };
+
+  $scope.addMessage = function (type, message) {
+    $('#alerts').append('<div class="alert alert-' + type + '">' + message + '</div>');
+  };
+
   $scope.updating = false;
 
   $scope.fetchMovieList = function (user_id) {
+    $('#alerts').html('');
     $scope.updating = true;
+    if (!$scope.check_user_id(user_id)) {
+      $scope.addMessage('warning', 'The entered user_id is not valid. The whole list will be fetched');
+    }
     $.getJSON('/api/' + user_id, function (data) {
       $scope.movies = data.todo; 
       $scope.updating = false;
